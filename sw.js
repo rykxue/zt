07bec2c821a91ec242d789b7625c27a22996b277
rykxue <rykxue.0fx7@gmail.com>
2025-01-14 23:51:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,56 @@
 const CACHE_NAME = 'ztype-game-v1';
 const ASSETS_TO_CACHE = [
-    '/',             // index.html
+    '/',
     '/index.html',
-    '/main.js',      // Main game logic
-    '/media/',       // All assets in the media directory
+    '/main.js',
+    '/media/background/stars.jpg',
+    '/media/background/gradient.png',
+    '/media/background/grid.png',
+    '/media/background/page.png',
+    '/media/fonts/avenir-36-white.png',
+    '/media/fonts/avenir-36-orange.png',
+    '/media/nfn-phoboslab-works.png',
+    '/media/ui/personal-best-badge.png',
+    '/media/fonts/avenir-36-blue.png',
+    '/media/ui/bar-blue.png',
+    '/media/title/ztype.png',
+    '/media/title/ship.png',
+    '/media/title/exhaust.png',
+    '/media/title/glitch-stripe.png',
+    '/media/title/glitch-code.png',
+    '/media/title/glitch-jpeg.png',
+    '/media/title/glitch-graph.png',
+    '/media/title/glitch-log.png',
+    '/media/sprites/explosion.png',
+    '/media/sprites/spark.png',
+    '/media/sprites/explosion-huge.jpg',
+    '/media/sprites/explosion-huge-glitch.jpg',
+    '/media/fonts/avenir-22-white-v2.png',
+    '/media/fonts/avenir-22-orange-v2.png',
+    '/media/sounds/hit.ogg',
+    '/media/sounds/target.ogg',
+    '/media/ui/reticle.png',
+    '/media/sprites/missle.png',
+    '/media/sounds/explosion-small.ogg',
+    '/media/sprites/mine.png',
+    '/media/sounds/explosion.ogg',
+    '/media/sprites/destroyer.png',
+    '/media/sounds/explosion-large.ogg',
+    '/media/sprites/oppressor.png',
+    '/media/sprites/bullet.png',
+    '/media/sprites/emp.png',
+    '/media/sounds/plasma.ogg',
+    '/media/sounds/click.ogg',
+    '/media/sprites/plasma.png',
+    '/media/ui/keyboard-background.png',
+    '/media/ui/emp-buttons.png',
+    '/media/ui/pause.png',
+    '/media/ui/key-edge-left.png',
+    '/media/ui/key-edge-right.png',
+    '/media/ui/key.png',
+    '/media/ui/multi-indicator.png',
+    '/media/sounds/multi-2.ogg',
+    '/media/sounds/multi-3.ogg',
 ];
 
 // Install event: Cache necessary files
@@ -28,7 +75,7 @@ self.addEventListener('fetch', (event) => {
                             cache.put(event.request, networkResponse.clone());
                         });
                     }
-                    return networkResponse;
+                    return networkResponse.clone();
                 })
             );
         })
